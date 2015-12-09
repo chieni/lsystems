03b75ffd0af74a8d27cd247c744fbfac00c90559
chieni <mitchieni@gmail.com>
2015-12-09 18:52:21 -0500

    Added cylinder rendering

--- index.js
+++ index.js
@@ -46,26 +46,26 @@
         // Quadratic Koch Island
         // var sys = LSystem('F+F+F+F', 90);
         // sys.addRule('F', 'F+F-F-FF+F+F-F');
-        // var points = sys.drawSystem3D(2, 30, new THREE.Vector3( -200,-200,0 ), 0);
+        // var results = sys.drawSystem(2, 30, new THREE.Vector3( -200,-200,0 ), 0);
 
 
         // Sierpinski Triangle
         // var sys = LSystem('F', 60);
         // sys.addRule('F', 'G-F-G');
         // sys.addRule('G', 'F+G+F');
-        // var points = sys.drawSystem2D(6, 10, new THREE.Vector3( -300,-300,0 ), 0);
+        // var results = sys.drawSystem(6, 10, new THREE.Vector3( -300,-300,0 ), 0);
 
         // Hexagonal Gopser
         // var sys = LSystem('F', 60);
         // sys.addRule('F', 'F+f++f-F--FF-f+');
         // sys.addRule('f', '-F+ff++f+F--F-f');
-        // var points = sys.drawSystem2D(4, 10, new THREE.Vector3( 0,-300,0 ), 10, 0);
+        // var results = sys.drawSystem(4, 10, new THREE.Vector3( 0,-300,0 ), 10, 0);
 
         // Dragon curve
         // var sys = LSystem('FX', 90);
         // sys.addRule('X', 'X+YF+');
         // sys.addRule('Y', '-FX-Y');
-        // var points = sys.drawSystem2D(10, 15, new THREE.Vector3(0,-250,0), 0);
+        // var results = sys.drawSystem(10, 15, new THREE.Vector3(0,-250,0), 0);
 
         // 3D Hilbert curve
         // var sys = LSystem('A', 90);
@@ -73,7 +73,7 @@
         // sys.addRule('B','A&F^CFB^F^D^^-F-D^|F^B|FC^F^A//');
         // sys.addRule('C','|D^|F^B-F+C^F^A&&FA&F^C+F+B^F^D//');
         // sys.addRule('D','|CFB-F+B|FA&F^A&&FB-F+B|FC//');
-        // var points = sys.drawSystem3D(3, 20, new THREE.Vector3( 0,100,0 ), 0);
+        // var results = sys.drawSystem(3, 50, new THREE.Vector3( 0,100,0 ), 0);
 
 
         // Plants
@@ -82,35 +82,36 @@
         // Tree1 
         // var sys = LSystem('F', 25.7);
         // sys.addRule('F', 'F[+F]F[-F]F');
-        // var points = sys.drawSystem3D(5, 3, new THREE.Vector3( 0,-400,0 ), 90);
+        // var results = sys.drawSystem(5, 3, new THREE.Vector3( 0,-400,0 ), 90);
+        
 
         // Tree2
-        // var sys = LSystem('F', 20);
-        // sys.addRule('F', 'F[+F]F[-F][F]');
-        // var points = sys.drawSystem2D(5, 13, new THREE.Vector3( 0,-400,0 ), 90);
+        var sys = LSystem('F', 20);
+        sys.addRule('F', 'F[+F]F[-F][F]');
+        var results = sys.drawSystem(5, 5, new THREE.Vector3( 0,-200,0 ), 90);
 
         // Tree3
         // var sys = LSystem('F', 22.5);
         // sys.addRule('F', 'FF-[-F+F+F]+[+F-F-F]');
-        // var points = sys.drawSystem2D(4, 13, new THREE.Vector3( 0,-400,0 ), 90);
+        // var results = sys.drawSystem(4, 13, new THREE.Vector3( 0,-400,0 ), 90);
 
         // Tree4
         // var sys = LSystem('X', 20);
         // sys.addRule('X', 'F[+X]F[-X]+X');
         // sys.addRule('F', 'FF');
-        // var points = sys.drawSystem2D(7, 3, new THREE.Vector3( 0,-400,0 ), 90);
+        // var results = sys.drawSystem(7, 3, new THREE.Vector3( 0,-400,0 ), 90);
 
         // Tree5
         // var sys = LSystem('X', 25.7);
         // sys.addRule('X', 'F[+X][-X]FX');
         // sys.addRule('F', 'FF');
-        // var points = sys.drawSystem2D(7, 3, new THREE.Vector3( 0,-400,0 ), 90);
+        // var results = sys.drawSystem(7, 3, new THREE.Vector3( 0,-400,0 ), 90);
 
         // Tree6
         // var sys = LSystem('X', 22.5);
         // sys.addRule('X', 'F-[[X]+X]+F[+FX]-X');
         // sys.addRule('F', 'FF');
-        // var points = sys.drawSystem2D(5, 7, new THREE.Vector3( 0,-300,0 ), 90);
+        // var results = sys.drawSystem(5, 7, new THREE.Vector3( 0,-300,0 ), 90);
 
         // 3D Tree
 
@@ -119,31 +120,74 @@
         // sys.addRule('F', 'S/////F');
         // sys.addRule('S', 'FL');
         // sys.addRule('L', '[^^-f+f+f-|-f+f+f]');
-        // var points = sys.drawSystem3D(5, 20, new THREE.Vector3( 0,-100,0 ), 0);
+        // var results = sys.drawSystem(5, 20, new THREE.Vector3( 0,-100,0 ), 0);
 
+        // var sys = LSystem('A', 90);
+        // var points = sys.hilbert3D( new THREE.Vector3( 0,0,0 ), 200.0, 0, 0, 1, 2, 3, 4, 5, 6, 7 );
 
         //
 
-        var colors3 = [];
-        for ( i = 0; i < points.length; i ++ ) {
-          geometry3.vertices.push( points[ i ] );
-          colors3[ i ] = new THREE.Color( 0xffffff );
-         // colors3[ i ].setHSL( i / points.length, 1.0, 0.5 );
-        }
+        //colors3[ i ].setHSL( i / points.length, 1.0, 0.5 );
 
-        geometry3.colors = colors3;
-
+        // geometry3.colors = results[1];
+        // geometry3.vertices = results[0];
         
-        material = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1, linewidth: 1, vertexColors: THREE.VertexColors } );
+        // material = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 1, linewidth: 1, vertexColors: THREE.VertexColors } );
 
-        var line, p, scale = 0.3, d = 225;
+        // var line, p, scale = 0.3, d = 225;
 
-        line = new THREE.Line(geometry3, material );
-        line.scale.x = line.scale.y = line.scale.z =  scale*1.5;
-        line.position.x = 0;
-        line.position.y = 0;
-        line.position.z = 0;
-        scene.add( line );
+        // line = new THREE.Line(geometry3, material );
+        // line.scale.x = line.scale.y = line.scale.z =  scale*1.5;
+        // line.position.x = 0;
+        // line.position.y = 0;
+        // line.position.z = 0;
+        // scene.add( line );
+
+
+        for (var i = 0; i<results[2].length; i++) {
+            scene.add(results[2][i]);
+        }
+        // for (var i=0; i<results[0].length-1; i++) {
+        //     var start = results[0][i];
+        //     var end = results[0][i+1];
+
+        //     var distance = start.distanceTo(end);
+        //     var angle = start.angleTo(end);
+
+        //     var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+        //     var cylinder = new THREE.CylinderGeometry(5,5,distance);
+        //     cylinder.applyMatrix(new THREE.Matrix4().makeRotationZ(angle));
+
+        //     var mesh = new THREE.Mesh(cylinder,material);
+        //     mesh.position.copy(start);
+        //     scene.add(mesh);
+        // }
+
+
+        // for (var i = 0; i<results[0].length-1; i++) {
+        //     var vstart = results[0][i];
+        //     var vend = results[0][i+1];
+
+        //     var HALF_PI = +Math.PI * .5;
+        //     var distance = vstart.distanceTo(vend);
+        //     var position  = vend.clone().addVectors(vend, vstart).divideScalar(2);
+
+        //     var material = new THREE.MeshLambertMaterial({color:0xffffff});
+        //     var cylinder = new THREE.CylinderGeometry(4,1,distance,10,10,false);
+
+        //     var orientation = new THREE.Matrix4();//a new orientation matrix to offset pivot
+        //     var offsetRotation = new THREE.Matrix4();//a matrix to fix pivot rotation
+        //     var offsetPosition = new THREE.Matrix4();//a matrix to fix pivot position
+        //     orientation.lookAt(vstart,vend,new THREE.Vector3(0,1,0));//look at destination
+        //     offsetRotation.makeRotationX(HALF_PI);//rotate 90 degs on X
+            
+        //     orientation.multiplyMatrices(orientation, offsetRotation);//combine orientation with rotation transformations
+        //     cylinder.applyMatrix(orientation)
+
+        //     var mesh = new THREE.Mesh(cylinder,material);
+        //     mesh.position=position;
+        //     scene.add(mesh);
+        // }
 
         //
 
--- lsystem.js
+++ lsystem.js
@@ -2,6 +2,7 @@
 var LSystem = function(axiom, theta) {
   var that = Object.create(LSystem.prototype);
   var rules = {};
+  var shrinkFactor = 1.8;
 
   that.addRule = function(character, rule) {
     rules[character] = rule;
@@ -25,128 +26,100 @@ var LSystem = function(axiom, theta) {
     return current;
   }
 
-  that.drawSystem2D = function(iterations, length, start, angle) {
+  that.drawSystem = function(iterations, length, start, angle) {
     var current = that.getCurrentSystem(iterations);
-    console.log(current)
+    var results = [];
+    var colors = [];
+
     var angle = angle*Math.PI / 180;
     var dtheta = theta*Math.PI / 180;
     var vertices = [];
+    var shapes = [];
+
 
     var vStack = [];
+    var dirStack = [];
     var aStack = [];
 
     var original = new THREE.Vector3(length, 0, 0);
+    var directionVector = new THREE.Vector3(1,0,0);
+    var initRotation = new THREE.Matrix4();
+    initRotation.makeRotationZ(angle);
+    directionVector.transformDirection(initRotation);
 
-    var prev = new THREE.Vector3();
     var begin = new THREE.Vector3(start.x,start.y,start.z);
     var end = new THREE.Vector3();
 
-    var zRotation = new THREE.Matrix4();
+    var cylRadius = 1.5;
 
     for (var i = 0; i<current.length; i++) {
       var character = current.charAt(i);
+      var yRotation = new THREE.Matrix4();
+      var xRotation = new THREE.Matrix4();
+      var zRotation = new THREE.Matrix4();
 
       switch (character) {
         case 'F':
         case 'G':
         case 'f':
-          // Rotate to theta, then go forward by 200
+          
           zRotation.makeRotationZ(angle);
-          var a = original.clone().transformDirection(zRotation).multiplyScalar(length);    
 
+          var a = directionVector.clone().setLength(length);
           end.addVectors(begin, a);  
          
           vertices.push(begin.clone());
           vertices.push(end.clone());
 
-          prev.copy(begin);
+          colors.push(new THREE.Color(0xffffff));
+          colors.push(new THREE.Color(0xffffff));
+
+
+          // Cylinder construction
+          var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+          var cylinder = new THREE.CylinderGeometry(cylRadius,cylRadius,length);
+
+          cylinder.applyMatrix(new THREE.Matrix4().makeTranslation(0, length/2, 0));
+          cylinder.applyMatrix(new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(90)));
+
+          var mesh = new THREE.Mesh(cylinder,material);
+          mesh.position.copy(begin);
+          mesh.lookAt(end);
+          shapes.push(mesh);
+
+
+
+
           begin.copy(end);
+
 
           break;
 
         case '-':
           angle -= dtheta;
-          break;
-
-        case '+':
-          angle += dtheta;
-          break;
-
-        case '[':
-          vStack.push(new THREE.Vector3(begin.x, begin.y, begin.z));
-          aStack.push(angle);
-          break;
-
-        case ']':
-          var temp = vStack.pop();
-          begin.copy(new THREE.Vector3(temp.x, temp.y, temp.z));
-          angle = aStack.pop();
-          break;
-
-        default:
-          break;
-      }
-
-    }
-    return vertices;
-  }
-
-  that.drawSystem3D = function(iterations, length, start, z0) {
-    var current = that.getCurrentSystem(iterations);
-    var dtheta = theta*Math.PI / 180;
-    
-    var zangle = z0*Math.PI / 180;
-    var yangle = 0;
-    var xangle = 0;
-
-    var vertices = [];
-    var vStack = [];
-
-    var dirStack = [];
-
-    var directionVector = new THREE.Vector3(1,0,0);
-    var initRotation = new THREE.Matrix4();
-    initRotation.makeRotationZ(zangle);
-    directionVector.transformDirection(initRotation);
-
-    var original = new THREE.Vector3(length, 0, 0);
-
-    var prev = new THREE.Vector3();
-    var begin = new THREE.Vector3(start.x,start.y,start.z);
-    var end = new THREE.Vector3();
-
-    var zRotation = new THREE.Matrix4();
-    var yRotation = new THREE.Matrix4();
-    var xRotation = new THREE.Matrix4();
-
-    for (var i = 0; i<current.length; i++) {
-      var character = current.charAt(i);
-
-      switch (character) {
-        case 'F':
-        case 'f':
-
-          var a = directionVector.clone().multiplyScalar(length);
-          end.addVectors(begin, a);  
-         
-          vertices.push(begin.clone());
-          vertices.push(end.clone());
-
-          prev.copy(begin);
-          begin.copy(end);
-
-          break;
-
-        case '-':
           zRotation.makeRotationZ(-dtheta);
           directionVector = directionVector.clone().transformDirection(zRotation);
           break;
 
         case '+':
-        console.log(zRotation)
+          angle += dtheta;
           zRotation.makeRotationZ(dtheta);
-          console.log(zRotation)
           directionVector = directionVector.clone().transformDirection(zRotation);
+          break;
+
+        case '[':
+          cylRadius /= shrinkFactor;
+          aStack.push(angle);
+          vStack.push(new THREE.Vector3(begin.x, begin.y, begin.z));
+          dirStack.push(new THREE.Vector3(directionVector.x, directionVector.y, directionVector.z));
+          break;
+
+        case ']':
+          cylRadius *= shrinkFactor;
+          angle = aStack.pop();
+          var temp = vStack.pop();
+          begin.copy(new THREE.Vector3(temp.x, temp.y, temp.z));
+          directionVector = dirStack.pop();
           break;
 
         case '\\':
@@ -174,53 +147,69 @@ var LSystem = function(axiom, theta) {
           zRotation.makeRotationZ(Math.PI);
           directionVector = directionVector.clone().transformDirection(zRotation);
           break;
-
-        case '[':
-          vStack.push(new THREE.Vector3(begin.x, begin.y, begin.z));
-          dirStack.push(new THREE.Vector3(directionVector.x, directionVector.y, directionVector.z));
-          break;
-
-        case ']':
-          var temp = vStack.pop();
-          begin.copy(new THREE.Vector3(temp.x, temp.y, temp.z));
-          directionVector = dirStack.pop();
-
-          break;
-
         default:
           break;
       }
 
     }
-    return vertices;
+    results.push(vertices);
+    results.push(colors);
+    results.push(shapes);
+    return results;
   }
-  that.hilbert = function(center, size, iterations, v0, v1, v2, v3 ){
-    var half = size / 2;
 
+  that.hilbert3D = function( center, size, iterations, v0, v1, v2, v3, v4, v5, v6, v7 ) {
+
+    // Default Vars
+    var center     = undefined !== center ? center : new THREE.Vector3( 0, 0, 0 ),
+      size       = undefined !== size ? size : 10,
+      half       = size / 2,
+      iterations = undefined !== iterations ? iterations : 1,
+      v0 = undefined !== v0 ? v0 : 0,
+      v1 = undefined !== v1 ? v1 : 1,
+      v2 = undefined !== v2 ? v2 : 2,
+      v3 = undefined !== v3 ? v3 : 3,
+      v4 = undefined !== v4 ? v4 : 4,
+      v5 = undefined !== v5 ? v5 : 5,
+      v6 = undefined !== v6 ? v6 : 6,
+      v7 = undefined !== v7 ? v7 : 7
+    ;
 
     var vec_s = [
-      new THREE.Vector3( center.x - half, center.y-half, center.z ),
-      new THREE.Vector3( center.x - half, center.y+half, center.z ),
-      new THREE.Vector3( center.x + half, center.y+half, center.z ),
-      new THREE.Vector3( center.x + half, center.y-half, center.z  )
+      new THREE.Vector3( center.x - half, center.y + half, center.z - half ),
+      new THREE.Vector3( center.x - half, center.y + half, center.z + half ),
+      new THREE.Vector3( center.x - half, center.y - half, center.z + half ),
+      new THREE.Vector3( center.x - half, center.y - half, center.z - half ),
+      new THREE.Vector3( center.x + half, center.y - half, center.z - half ),
+      new THREE.Vector3( center.x + half, center.y - half, center.z + half ),
+      new THREE.Vector3( center.x + half, center.y + half, center.z + half ),
+      new THREE.Vector3( center.x + half, center.y + half, center.z - half )
     ];
 
     var vec = [
       vec_s[ v0 ],
       vec_s[ v1 ],
       vec_s[ v2 ],
-      vec_s[ v3 ]
+      vec_s[ v3 ],
+      vec_s[ v4 ],
+      vec_s[ v5 ],
+      vec_s[ v6 ],
+      vec_s[ v7 ]
     ];
 
     // Recurse iterations
-    if ( 0 <= -- iterations ) {
+    if ( -- iterations >= 0 ) {
 
       var tmp = [];
 
-      Array.prototype.push.apply( tmp, that.hilbert ( vec[ 0 ], half, iterations, v0, v3, v2, v1 ) );
-      Array.prototype.push.apply( tmp, that.hilbert( vec[ 1 ], half, iterations, v0, v1, v2, v3 ) );
-      Array.prototype.push.apply( tmp, that.hilbert( vec[ 2 ], half, iterations, v0, v1, v2, v3 ) );
-      Array.prototype.push.apply( tmp, that.hilbert( vec[ 3 ], half, iterations, v2, v1, v0, v3 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 0 ], half, iterations, v0, v3, v4, v7, v6, v5, v2, v1 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 1 ], half, iterations, v0, v7, v6, v1, v2, v5, v4, v3 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 2 ], half, iterations, v0, v7, v6, v1, v2, v5, v4, v3 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 3 ], half, iterations, v2, v3, v0, v1, v6, v7, v4, v5 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 4 ], half, iterations, v2, v3, v0, v1, v6, v7, v4, v5 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 5 ], half, iterations, v4, v3, v2, v5, v6, v1, v0, v7 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 6 ], half, iterations, v4, v3, v2, v5, v6, v1, v0, v7 ) );
+      Array.prototype.push.apply( tmp, that.hilbert3D ( vec[ 7 ], half, iterations, v6, v5, v2, v1, v0, v3, v4, v7 ) );
 
       // Return recursive call
       return tmp;
@@ -229,8 +218,8 @@ var LSystem = function(axiom, theta) {
 
     // Return complete Hilbert Curve.
     return vec;
-  }
 
+  }
   Object.freeze(that);
   return that;
 }
